bdace77585c8fdb215205323697c8a8c3e8cc3b5
zhuoqiw <zhuoqiw@hotmail.com>
2021-12-01 17:52:59 +0800

    Fix typo

--- pylon-config.cmake
+++ pylon-config.cmake
@@ -1,28 +1,28 @@
 if(NOT TARGET pylon::base)
-  add_library(pylon::pylon_base SHARED IMPORTED)
-  set_target_properties(pylon::pylon_base PROPERTIES
+  add_library(pylon::base SHARED IMPORTED)
+  set_target_properties(pylon::base PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/include"
     IMPORTED_LOCATION "${CMAKE_CURRENT_LIST_DIR}/lib/libpylonbase-6.2.0.so"
   )
 endif()
 
 if(NOT TARGET pylon::utility)
-  add_library(pylon::pylon_utility SHARED IMPORTED)
-  set_target_properties(pylon::pylon_utility PROPERTIES
+  add_library(pylon::utility SHARED IMPORTED)
+  set_target_properties(pylon::utility PROPERTIES
     IMPORTED_LOCATION "${CMAKE_CURRENT_LIST_DIR}/lib/libpylonutility-6.2.0.so"
   )
 endif()
 
 if(NOT TARGET pylon::gen)
-  add_library(pylon::pylon_gen SHARED IMPORTED)
-  set_target_properties(pylon::pylon_gen PROPERTIES
+  add_library(pylon::gen SHARED IMPORTED)
+  set_target_properties(pylon::gen PROPERTIES
     IMPORTED_LOCATION "${CMAKE_CURRENT_LIST_DIR}/lib/libGenApi_gcc_v3_1_Basler_pylon.so"
   )
 endif()
 
 if(NOT TARGET pylon::gc)
-  add_library(pylon::pylon_gc SHARED IMPORTED)
-  set_target_properties(pylon::pylon_gc PROPERTIES
+  add_library(pylon::gc SHARED IMPORTED)
+  set_target_properties(pylon::gc PROPERTIES
     IMPORTED_LOCATION "${CMAKE_CURRENT_LIST_DIR}/lib/libGCBase_gcc_v3_1_Basler_pylon.so"
   )
 endif()
